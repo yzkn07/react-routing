b82b6c793909105e122657e86ad69016d88ab62d
yzkn07 <mehmetali.1992@hotmail.com>
2024-06-09 13:32:09 +0300

    router eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import useAuth from './store/context'
+// import useAuth from './store/context'
+// import './App.css'
+
+// function App() {
+
+//   const { name, setName} = useAuth();
+
+//   console.log(name);
+
+//   return (
+//     <>
+//       <input type='text' 
+//       readOnly
+//       value={name} 
+//       onChange={e => setName(e.target.value)}
+//       ></input>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+// Router
+
 import './App.css'
 
 function App() {
 
-  const { name, setName} = useAuth();
-
-  console.log(name);
-
   return (
     <>
-      <input type='text' 
-      // readOnly
-      value={name} 
-      onChange={e => setName(e.target.value)}
-      ></input>
+      <h1>merhaba</h1>
     </>
   )
 }
-
 export default App
